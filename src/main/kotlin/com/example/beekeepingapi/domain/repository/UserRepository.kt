22e38a752271553282b89ac06e03cf3395d60098
Chepkasov Ruslan <chepkasov.ruslan01@gmail.com>
2023-12-13 19:27:47 +0300

    delete unused import from UserRepository.kt

--- src/main/kotlin/com/example/beekeepingapi/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/example/beekeepingapi/domain/repository/UserRepository.kt
@@ -2,7 +2,6 @@ package com.example.beekeepingapi.domain.repository
 
 import com.example.beekeepingapi.domain.entity.User
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-import reactor.core.publisher.Mono
 
 interface UserRepository : CoroutineCrudRepository<User, Int> {
 
